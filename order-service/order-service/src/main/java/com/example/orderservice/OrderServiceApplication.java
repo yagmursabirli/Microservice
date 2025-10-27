e91d3a17b8e1f77b803793215a445efa6322bbf1
melikeaytac <48melikeaytac@gmail.com>
2025-10-27 18:08:48 +0300

    fixed comments

--- order-service/order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -2,7 +2,7 @@ package com.example.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients; // YENİ IMPORT
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
